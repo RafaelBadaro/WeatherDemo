7945044ef93a88d0ad0dc191e3b290a3a9c30d37
Rafael Badaro <rafabadaro.15@gmail.com>
2023-10-23 19:24:44 -0300

    tudo

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Rafael Badaró on 22/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
